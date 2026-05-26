1b378b9771cc990062a3c9eafe8fe6f7d3c6ed5f
Srinivas Vellala <srinivasvl81@gmail.com>
2026-05-26 11:50:40 +0200

    added unit test: test_daily_max_integers

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,7 @@
 import numpy as np
 import numpy.testing as npt
 
-from inflammation.models import daily_mean
+from inflammation.models import daily_mean, daily_max
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -28,4 +28,14 @@ def test_daily_mean_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
+
+def test_daily_max_integers():
+    """ Tests that the max function works for an array of negative integers. """
+    test_input = np.array([[1, -2],
+                           [3, -4],
+                           [5, -6]])
+    test_result = np.array([5, -2])
+
+    # Need to use Numpy testing functions to find max value in axis=0 of the array
+    npt.assert_array_equal(daily_max(test_input), test_result)
 
