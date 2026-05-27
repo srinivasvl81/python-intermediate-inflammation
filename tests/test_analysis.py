dbffbe843ea0a9a2253066ee212efb74d035c12e
Srinivas Vellala <srinivasvl81@gmail.com>
2026-05-27 13:30:16 +0200

    added test_standard_deviation in test_analysis.py

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -2,6 +2,8 @@ from inflammation.analysis import analyse_data
 import numpy.testing as npt
 import numpy as np
 import os
+import pytest
+import math
 
 
 def test_analyse_data():
@@ -17,3 +19,17 @@ def test_analyse_data():
                             ,0.50323031,0.47574665,0.45197398,0.22070227]
 
     npt.assert_almost_equal(result, expected_result)
+
+
+@pytest.mark.parametrize('data,expected_output', [
+    ([[[0, 1, 0], [0, 2, 0]]], [0, 0, 0]),
+    ([[[0, 2, 0]], [[0, 1, 0]]], [0, math.sqrt(0.25), 0]),
+    ([[[0, 1, 0], [0, 2, 0]], [[0, 1, 0], [0, 2, 0]]], [0, 0, 0])
+],
+ids=['Two patients in same file', 'Two patients in different files', 'Two identical patients in two different files'])
+
+def test_compute_standard_deviation_by_day(data, expected_output):
+    from inflammation.analysis import compute_standard_deviation_by_day
+
+    result = compute_standard_deviation_by_day(data)
+    npt.assert_array_almost_equal(result, expected_output)
